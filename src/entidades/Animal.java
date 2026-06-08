ee9dc156acdb86417d8d6e9d50e37dcc473208c2
dudalombardi2011 <dudalombardi2011@gmail.com>
2026-06-08 17:02:47 -0300

    adicionando arquivos

--- src/entidades/Animal.java
+++ src/entidades/Animal.java
@@ -4,4 +4,37 @@ public abstract class Animal{
     private String raca;
     private int idade;
 
+    public Animal(int id, String nome, String raca, int idade) {
+        this.id = id;
+        this.nome = nome;
+        this.raca = raca;
+        this.idade = idade;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getRaca() {
+        return raca;
+    }
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    
 }
