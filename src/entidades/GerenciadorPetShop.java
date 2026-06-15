1e18d50ada4a25484f467577a7fddb02ebf1abfa
Artur Silveira <arturdcsilveira@gmail.com>
2026-06-15 20:23:32 -0300

    Começo da lógica da Main

--- src/entidades/GerenciadorPetShop.java
+++ src/entidades/GerenciadorPetShop.java
@@ -1,4 +1,6 @@
 package entidades;
+import entidades.Porte;
+
 import java.util.ArrayList;
 
 public class GerenciadorPetShop {
@@ -31,8 +33,43 @@ public class GerenciadorPetShop {
         this.compras = compras;
     }
 
-    
+    public void carregarDadosIniciais() {
+        // 1. Criando 7 Clientes
+        Cliente c1 = new Cliente(1, "Duda Lombardi", 11111);
+        Cliente c2 = new Cliente(2, "Artur Silveira", 22222);
+        Cliente c3 = new Cliente(3, "Artur 'Certinho'", 33333);
+        Cliente c4 = new Cliente(4, "Israel Gabriel", 44444);
+        Cliente c5 = new Cliente(5, "João Lisboa", 55555);
+        Cliente c6 = new Cliente(6, "Antonio Nunes", 66666);
+        Cliente c7 = new Cliente(7, "Clara dos Anjos", 77777);
 
-    //RegistrarCompra  
+        // 2. Criando 7 Animais (int id, String nome, String especie, String raca, int idade, Porte porte)
+        Animal a1 = new Animal(1, "Apolo","Cachorro","Border Collie",6, Porte.MEDIO);
+        Animal a2 = new Animal(1, "Stela","Cachorro","Border Collie",5, Porte.MEDIO);
+        Animal a3 = new Animal(2, "Mimi", "Gato", "Siamês", 2, Porte.PEQUENO);
+        Animal a4 = new Animal(3, "Bob", "Cachorro", "Pug", 4, Porte.PEQUENO);
+        Animal a5 = new Animal(4, "Astolfo", "Ave", "Periquito", 1, Porte.PEQUENO);
+        Animal a6 = new Animal(5, "Luna", "Gato", "Persa", 5, Porte.PEQUENO);
+        Animal a7 = new Animal(6, "Max", "Cachorro","Bulldog", 2, Porte.MEDIO);
+        Animal a8 = new Animal(7, "Mel", "Cachorro", "Poodle", 6, Porte.PEQUENO);
+
+        c1.adicionarAnimal(a1);
+        c1.adicionarAnimal(a2);
+        c2.adicionarAnimal(a3);
+        c3.adicionarAnimal(a4);
+        c4.adicionarAnimal(a5);
+        c5.adicionarAnimal(a6);
+        c6.adicionarAnimal(a7);
+        c7.adicionarAnimal(a8);
+
+        // Adicionando os clientes na lista da PRÓPRIA classe
+        this.cadastrarCliente(c1);
+        this.cadastrarCliente(c2);
+        this.cadastrarCliente(c3);
+        this.cadastrarCliente(c4);
+        this.cadastrarCliente(c5);
+        this.cadastrarCliente(c6);
+        this.cadastrarCliente(c7);
+    }
 
 }
--- src/main/Programa.java
+++ src/main/Programa.java
@@ -1,13 +1,58 @@
 package main;
 
 import entidades.GerenciadorPetShop;
-import entidades.Cliente;
 import java.util.Scanner;
 
 public class Programa {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         GerenciadorPetShop petGremio = new GerenciadorPetShop();
+
+        petGremio.carregarDadosIniciais();
+
+        int opcao = 0;
+
+        while (opcao != 6) {
+            System.out.println("\n==================================");
+            System.out.println("       SISTEMA PET SHOP           ");
+            System.out.println("==================================");
+            System.out.println("1 - Incluir (Cadastros)");
+            System.out.println("2 - Alterar");
+            System.out.println("3 - Excluir");
+            System.out.println("4 - Listar (Relatórios)");
+            System.out.println("5 - Buscar por Atributo");
+            System.out.println("6 - Sair");
+            System.out.print("Escolha uma opção: ");
+
+            opcao = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("\n--- MENU INCLUIR ---");
+                    // gerenciador.incluirDados(sc);
+                    break;
+                case 2:
+                    System.out.println("\n--- MENU ALTERAR ---");
+                    break;
+                case 3:
+                    System.out.println("\n--- MENU EXCLUIR ---");
+                    break;
+                case 4:
+                    System.out.println("\n--- MENU LISTAR ---");
+                    break;
+                case 5:
+                    System.out.println("\n--- MENU BUSCAR ---");
+                    break;
+                case 6:
+                    System.out.println("\nSaindo do sistema... Até logo!");
+                    break;
+                default:
+                    System.out.println("\nOpção inválida! Tente novamente.");
+            }
+        }
+
+        sc.close();
     }
 }
